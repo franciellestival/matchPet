e4b41dc35f0addb9602c08ec6a1a4f96b4e2a860
Francielle Mazzarotto Stival <franciellestival@gmail.com>
2022-11-05 21:19:47 -0300

    refactor: pet register page

--- modules/pet_profile/lib/pages/pet_register_page.dart
+++ modules/pet_profile/lib/pages/pet_register_page.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_svg/svg.dart';
 import 'package:theme/components/button_component.dart';
 
 import 'package:theme/components/form_components.dart';
+import 'package:theme/components/image_form_field.dart';
+import 'package:theme/components/spacer.dart';
 import 'package:theme/layout/app_assets.dart';
 import 'package:theme/layout/app_config.dart';
+import 'package:image_form_field/image_form_field.dart';
 
 enum Sexo { MACHO, FEMEA }
 
@@ -16,15 +21,11 @@ enum Especie { CAO, GATO, PASSARO, ROEDOR, OUTRO }
 enum Status { DISPONIVEL, ADOTADO, DESAPARECIDO }
 
 class PetRegisterPage extends StatelessWidget {
-  final List<String> sexo = Sexo.values.map((e) => e.toString()).toList();
-  final List<String> porte = Porte.values.map((e) => e.toString()).toList();
-  final List<String> especie = Especie.values.map((e) => e.toString()).toList();
-  final List<String> status = Status.values.map((e) => e.toString()).toList();
+  static const heightSpacer = HeightSpacer();
 
   PetRegisterPage({super.key});
 
   final _formKey = GlobalKey<FormState>();
-  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +34,13 @@ class PetRegisterPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const SizedBox(height: 10),
+            heightSpacer,
             const Text(
               'Cadastro de Pet',
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 20),
+            heightSpacer,
             Center(
               child: SvgPicture.asset(
                 AppSvgs.appIcon,
@@ -47,41 +48,60 @@ class PetRegisterPage extends StatelessWidget {
                 width: 100,
               ),
             ),
-            const SizedBox(height: 20),
+            heightSpacer,
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Form(
-                key: _formKey,
-                child: Wrap(
-                  runSpacing: 22,
-                  children: <Widget>[
-                    const Text(
-                      'Informações Gerais',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    FormInputBox(hintText: 'Nome', controller: controller),
-                    FormDropDownInput(items: especie, hintText: 'Espécie'),
-                    FormDropDownInput(
-                        items: sexo.map((e) => e.toString()).toList(),
-                        hintText: 'Sexo'),
-                    FormDropDownInput(items: porte, hintText: 'Porte'),
-                    FormDropDownInput(items: status, hintText: 'Status'),
-                    FormInputBox(
-                        hintText: 'Localização', controller: controller),
-                    FormInputBox(hintText: 'Raça', controller: controller),
-                    FormInputBox(hintText: 'Idade', controller: controller),
-                    FormInputBox(hintText: 'Peso', controller: controller),
-                    FormInputBox(hintText: 'Descrição', controller: controller),
-                  ],
-                ),
-              ),
+              child: _buildPetRegisterForm(_formKey),
             ),
-            const SizedBox(height: 30),
+            heightSpacer,
             PrimaryButton(onTap: () {}, text: 'Salvar'),
-            const SizedBox(height: 30)
+            heightSpacer
           ],
         ),
       ),
     );
   }
+}
+
+Form _buildPetRegisterForm(GlobalKey<FormState> formKey) {
+  final List<String> sexo = Sexo.values.map((e) => e.toString()).toList();
+  final List<String> porte = Porte.values.map((e) => e.toString()).toList();
+  final List<String> especie = Especie.values.map((e) => e.toString()).toList();
+  final List<String> status = Status.values.map((e) => e.toString()).toList();
+
+  final controller = TextEditingController();
+
+  return Form(
+    key: formKey,
+    child: Wrap(
+      runSpacing: 22,
+      children: <Widget>[
+        ImageFormField<ImageInputAdapter>(
+            //previewImageBuilder: (_, ImageInputAdapter image) => image.widgetize(),
+            buttonBuilder: (_, int count) => Container(
+                child: Text(count == null || count < 1
+                    ? "Upload Image"
+                    : "Upload More"))
+            //initializeFileAsImage: (File file) => ImageInputAdapter(file: file),
+            //initialValue: existingPhotoUrl == null ? null : (List<ImageInputImageAdapter>()..add(ImageInputImageAdapter(url: existingPhotoUrl))),
+            // Even if `shouldAllowMultiple` is true, images will always be a `List` of the declared type (i.e. `ImageInputAdater`).
+            //onSaved: (images) _images = images,
+            ),
+        const Text(
+          'Informações Gerais',
+          style: TextStyle(fontSize: 16),
+        ),
+        FormInputBox(hintText: 'Nome', controller: controller),
+        FormDropDownInput(items: especie, hintText: 'Espécie'),
+        FormDropDownInput(items: sexo, hintText: 'Sexo'),
+        FormDropDownInput(items: porte, hintText: 'Porte'),
+        FormDropDownInput(items: status, hintText: 'Status'),
+        FormInputBox(hintText: 'Localização', controller: controller),
+        FormInputBox(hintText: 'Raça', controller: controller),
+        FormInputBox(hintText: 'Idade', controller: controller),
+        FormInputBox(hintText: 'Peso', controller: controller),
+        FormInputBox(hintText: 'Descrição', controller: controller),
+      ],
+    ),
+  );
 }
---
+++ modules/theme/lib/components/spacer.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class HeightSpacer extends StatelessWidget {
+  final double? height;
+
+  // ignore: use_key_in_widget_constructors
+  const HeightSpacer({
+    height,
+  }) : height = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: height);
+  }
+}
+
+class WidthSpacer extends StatelessWidget {
+  final double? width;
+
+  // ignore: use_key_in_widget_constructors
+  const WidthSpacer({
+    width,
+  }) : width = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: width);
+  }
+}
