a6a97fe1e415cde531b6a6ef24814bad8d942ec9
Francielle Mazzarotto Stival <franciellestival@gmail.com>
2023-01-15 14:50:32 -0300

    layout: edit pet Page

--- modules/pet_profile/lib/pages/pet_profile_page.dart
+++ modules/pet_profile/lib/pages/pet_profile_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:matchpet/routes/app_routes.dart';
 import 'package:pet_profile/controller/pet_controller.dart';
+import 'package:pet_profile/models/pet_profile.dart';
 
 import 'package:theme/export_theme.dart';
 
@@ -17,6 +18,7 @@ class PetProfilePage extends StatefulWidget {
 class _PetProfilePageState extends State<PetProfilePage> {
   //formKey set up
   final _formKey = GlobalKey<FormState>();
+  PetProfile pet = Get.arguments;
 
   //controllers setup
   final _nameController = TextEditingController();
@@ -25,22 +27,15 @@ class _PetProfilePageState extends State<PetProfilePage> {
   final _ageController = TextEditingController();
   final _weightController = TextEditingController();
   final _descriptionController = TextEditingController();
-  final _imageController = Get.put<ImageController>(ImageController());
+  final _speciesController = TextEditingController();
+  final _genderController = TextEditingController();
+  final _sizeController = TextEditingController();
+  final _statusController = TextEditingController();
+  final _neuteredController = TextEditingController();
+  final _specialNeeds = TextEditingController();
   Rx<bool> inputEnabled = Rx<bool>(false);
 
   //DropDown menu and values set up
-  late List<String> species;
-  Rx<String> speciesCurrentValue = ''.obs;
-
-  late List<String> genders;
-  Rx<String> genderCurrentValue = ''.obs;
-
-  late List<String> sizes;
-  Rx<String> sizeCurrentValue = ''.obs;
-
-  late List<String> status;
-  Rx<String> statusCurrentValue = ''.obs;
-
   final List<String> neutered = ['Sim', 'Não'];
   Rx<String> neuteredCurrentValue = ''.obs;
 
@@ -48,80 +43,200 @@ class _PetProfilePageState extends State<PetProfilePage> {
   Rx<String> specialNeedsCurrentValue = ''.obs;
 
   Form _buildPetRegisterForm(GlobalKey<FormState> formKey) {
+    //controllers setup
+    _nameController.text = pet.name ?? '';
+    _locationController.text = pet.location?.address ?? '';
+    _breedController.text = pet.breed ?? '';
+    _ageController.text = pet.age?.toString() ?? '';
+    _weightController.text = pet.weight?.toString() ?? '';
+    _descriptionController.text = pet.description ?? '';
+    _genderController.text = pet.gender?.displayName ?? '';
+    _sizeController.text = pet.size?.displayName ?? '';
+    _neuteredController.text = pet.neutered ?? false ? 'Sim' : 'Não';
+    _statusController.text = pet.status?.displayName ?? '';
+    _specialNeeds.text = pet.specialNeeds ?? false ? 'Sim' : 'Não';
+
+    Rx<String> speciesCurrentValue = ''.obs;
+    Rx<String> genderCurrentValue = ''.obs;
+    Rx<String> sizeCurrentValue = ''.obs;
+    Rx<String> statusCurrentValue = ''.obs;
+
     return Form(
       key: formKey,
       child: Wrap(
         runSpacing: 22,
         children: <Widget>[
-          ImageInput(placeHolderPath: AppSvgs.pawIcon),
           const Text(
             'Informações Gerais',
             style: TextStyle(fontSize: 16),
           ),
           FormInputBox(
+            backgroundColor: inputBackgoundColor(),
+            enable: inputEnabled.value,
             hintText: 'Nome',
             controller: _nameController,
           ),
-          FormDropDownInput(
-            child: DropDownItem(
-              currentValue: speciesCurrentValue,
-              items: species,
-              hintText: 'Espécie',
-            ),
-          ),
-          FormDropDownInput(
-            child: DropDownItem(
-              currentValue: genderCurrentValue,
-              items: genders,
-              hintText: 'Sexo',
-            ),
-          ),
-          FormDropDownInput(
-            child: DropDownItem(
-              currentValue: sizeCurrentValue,
-              items: sizes,
-              hintText: 'Porte',
-            ),
-          ),
-          FormDropDownInput(
-            child: DropDownItem(
-                currentValue: statusCurrentValue,
-                items: status,
-                hintText: 'Status'),
-          ),
-          FormDropDownInput(
-            child: DropDownItem(
-              currentValue: neuteredCurrentValue,
-              items: neutered,
-              hintText: 'Castrado(a)?',
-            ),
-          ),
-          FormDropDownInput(
-            child: DropDownItem(
-              currentValue: specialNeedsCurrentValue,
-              items: specialNeeds,
-              hintText: 'Necessidades Especiais?',
-            ),
-          ),
+          inputEnabled.value
+              ? FutureBuilder<List<String?>>(
+                  future: PetController.species(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      var data = snapshot.data!;
+                      return FormDropDownInput(
+                          child: DropDownItem(
+                        items: data,
+                        currentValue: speciesCurrentValue,
+                        hintText: 'Espécie',
+                      ));
+                    } else {
+                      if (snapshot.hasError) {
+                        Get.snackbar('Error', snapshot.error.toString());
+                      }
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                )
+              : FormInputBox(
+                  backgroundColor: inputBackgoundColor(),
+                  enable: false,
+                  hintText: 'Espécie',
+                  controller: _speciesController,
+                ),
+          inputEnabled.value
+              ? FutureBuilder<List<String?>>(
+                  future: PetController.genders(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      var data = snapshot.data!;
+                      return FormDropDownInput(
+                          child: DropDownItem(
+                        items: data,
+                        currentValue: genderCurrentValue,
+                        hintText: 'Sexo',
+                      ));
+                    } else {
+                      if (snapshot.hasError) {
+                        Get.snackbar('Error', snapshot.error.toString());
+                      }
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                )
+              : FormInputBox(
+                  backgroundColor: inputBackgoundColor(),
+                  enable: false,
+                  hintText: 'Sexo',
+                  controller: _genderController,
+                ),
+          inputEnabled.value
+              ? FutureBuilder<List<String?>>(
+                  future: PetController.sizes(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      var data = snapshot.data!;
+                      return FormDropDownInput(
+                          child: DropDownItem(
+                        items: data,
+                        currentValue: sizeCurrentValue,
+                        hintText: 'Porte',
+                      ));
+                    } else {
+                      if (snapshot.hasError) {
+                        Get.snackbar('Error', snapshot.error.toString());
+                      }
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                )
+              : FormInputBox(
+                  backgroundColor: inputBackgoundColor(),
+                  enable: false,
+                  hintText: 'Porte',
+                  controller: _sizeController,
+                ),
+          inputEnabled.value
+              ? FutureBuilder<List<String?>>(
+                  future: PetController.status(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      var data = snapshot.data!;
+                      return FormDropDownInput(
+                          child: DropDownItem(
+                        items: data,
+                        currentValue: statusCurrentValue,
+                        hintText: 'Status',
+                      ));
+                    } else {
+                      if (snapshot.hasError) {
+                        Get.snackbar('Error', snapshot.error.toString());
+                      }
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                )
+              : FormInputBox(
+                  backgroundColor: inputBackgoundColor(),
+                  enable: false,
+                  hintText: 'Status',
+                  controller: _statusController,
+                ),
+          inputEnabled.value
+              ? FormDropDownInput(
+                  child: DropDownItem(
+                    currentValue: neuteredCurrentValue,
+                    items: neutered,
+                    hintText: 'Castrado(a)?',
+                  ),
+                )
+              : FormInputBox(
+                  backgroundColor: inputBackgoundColor(),
+                  enable: false,
+                  hintText: 'Castrado(a)',
+                  controller: _neuteredController,
+                ),
+          inputEnabled.value
+              ? FormDropDownInput(
+                  child: DropDownItem(
+                    currentValue: specialNeedsCurrentValue,
+                    items: specialNeeds,
+                    hintText: 'Necessidades Especiais?',
+                  ),
+                )
+              : FormInputBox(
+                  backgroundColor: inputBackgoundColor(),
+                  enable: false,
+                  hintText: 'Necessidades Especiais',
+                  controller: _specialNeeds,
+                ),
           FormInputBox(
+            backgroundColor: inputBackgoundColor(),
+            enable: inputEnabled.value,
             hintText: 'Localização',
             controller: _locationController,
           ),
           FormInputBox(
+            backgroundColor: inputBackgoundColor(),
+            enable: inputEnabled.value,
             hintText: 'Raça',
             controller: _breedController,
           ),
           FormInputBox(
+            backgroundColor: inputBackgoundColor(),
+            enable: inputEnabled.value,
             hintText: 'Idade',
             controller: _ageController,
             textInputType: TextInputType.number,
           ),
           FormInputBox(
+            backgroundColor: inputBackgoundColor(),
+            enable: inputEnabled.value,
             hintText: 'Peso',
             controller: _weightController,
             textInputType: TextInputType.number,
           ),
           FormInputBox(
+            backgroundColor: inputBackgoundColor(),
+            enable: inputEnabled.value,
             hintText: 'Descrição',
             controller: _descriptionController,
           ),
@@ -162,47 +277,27 @@ class _PetProfilePageState extends State<PetProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const GenericAppBar(title: 'Cadastrar Pet'),
-      backgroundColor: AppColors.primaryColor,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            PetProfilePage.heightSpacer,
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: _buildPetRegisterForm(_formKey),
-            ),
-            PetProfilePage.heightSpacer,
-            PrimaryButton(onTap: _registerPet, text: 'Salvar'),
-            PetProfilePage.heightSpacer
-          ],
+    return Obx(() {
+      return Scaffold(
+        appBar: const GenericAppBar(title: 'Editar Pet'),
+        backgroundColor: AppColors.primaryColor,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              PetProfilePage.heightSpacer,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: _buildPetRegisterForm(_formKey),
+              ),
+              PetProfilePage.heightSpacer,
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 
-  Future<void> _registerPet() async {
-    if (_formKey.currentState!.validate()) {
-      try {
-        await PetController.registerPet(
-            _nameController.text,
-            speciesCurrentValue.value,
-            genderCurrentValue.value,
-            sizeCurrentValue.value,
-            statusCurrentValue.value,
-            _breedController.text,
-            int.parse(_breedController.text),
-            double.parse(_weightController.text),
-            _descriptionController.text,
-            neuteredCurrentValue.value == 'Sim' ? true : false,
-            specialNeedsCurrentValue.value == 'Sim' ? true : false,
-            _imageController.imagePath.value);
-        Get.offAndToNamed(Routes.statusRoute);
-      } on Exception catch (e) {
-        Get.snackbar("Erro!", e.toString(),
-            duration: const Duration(seconds: 5));
-      }
-    }
+  Color? inputBackgoundColor() {
+    return inputEnabled.value ? null : Colors.grey.withOpacity(0.5);
   }
 }
