c2f2e130efbf6da50a49f7189d1b7b4e59f49961
Francielle Mazzarotto Stival <franciellestival@gmail.com>
2023-01-15 16:03:25 -0300

    ultimo commit

--- app/lib/routes/app_pages.dart
+++ app/lib/routes/app_pages.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:pet_profile/pages/pet_details_page.dart';
 
 import 'package:pet_profile/pages/pet_list_page.dart';
+import 'package:pet_profile/pages/pet_profile_page.dart';
 import 'package:pet_profile/pages/pet_register_page.dart';
 import 'package:user_profile/pages/login_page.dart';
 import 'package:user_profile/pages/profile_page.dart';
@@ -51,6 +52,10 @@ class AppPages {
     GetPage(
       name: Routes.home,
       page: () => CustomBottomNavBar(),
+    ),
+    GetPage(
+      name: Routes.petEditPage,
+      page: () => PetProfilePage(),
     ),
   ];
 }
